3cddcdc359ad2740c264490b452b9bfc02e0e900
Silvia Liu <silvia.shuchang.liu@gmail.com>
2018-02-20 17:49:29 -0500

    Update SVlearning.r for the pipeline

--- scripts/SVlearning.r
+++ scripts/SVlearning.r
@@ -1,56 +1,30 @@
 ###  main script for SVlearning
 
-module load gcc/4.9.2
-module load bedtools/2.27.0
-module load R/3.4.1
-cd /projects/liusi/DP/Gitpackage/SVlearning/scripts/
+## system setting
+# module load gcc/4.9.2
+# module load bedtools/2.27.0
+# module load R/3.4.1
+# cd /projects/liusi/DP/Gitpackage/SVlearning/scripts/
 
   
 rm(list=ls())
 
+## load source function
+source(paste(para$scriptDir,"/Rsource.r",sep=""))
+
 ## read in config file to set parameters
 #args=commandArgs(TRUE)
 #configFileName=args[1]
-configFileName="../example/code/config"  ## change this part later-on
-configFile=read.table(configFileName,comment.char="#",sep="\t",as.is=T,header=F)
+configFileName="../example/code/config"  ## change here, for testing 
 
-para=list()
-for(i in 1:nrow(configFile)){
-  para[[configFile[i,1]]]=unlist(strsplit(gsub(" ","",configFile[i,2]),split=","))
-}
+## check and format the input paramter
+para=checkFormatPara(configFileName)
 
-# change format
-para$Nrate=as.numeric(para$Nrate)
-para$vote=as.numeric(para$vote)
-para$DELbin=as.integer(para$DELbin)
-para$DUPbin=as.integer(para$DUPbin)
-para$Chr=as.character(para$Chr)
-
-# ref len file
-para$refLen=rep(0,times=length(para$Chr))
-names(para$refLen)=para$Chr
-tmp=read.table(para$refLenFile,sep="\t",header=F,as.is=T)
-for(chr in para$Chr){
-  para$refLen[chr]=as.integer(tmp[which(tmp[,1]==chr),2])
-}
-
-## training sample, change here in case don't want to use the full training samples
-para$trainSample=list.files(para$trainDataDir)
-## testing sample, change here in case don't want to use the full testing samples
-para$testSample=list.files(para$testDataDir)
-
-
-print(para)
-
-## load source function
-source(paste(para$scriptDir,"/Rscource.r",sep=""))
-## TBD load in python function
+## install R package 
+installRpackage(para)
 
 ## prepare data from vcf to python input file
 prepareData(para)
-
-## SV -> piece-SV by python code
-# call python code
 
 ## piece-SV data format
 formatPieceSV(para)
@@ -58,13 +32,17 @@ formatPieceSV(para)
 ## train the model or not
 if(para$knownModel==FALSE){
   # no known model, need to train the model
-  trainModel(para)
+  set.seed(06032)
+  trainModel(para,Sample=para$trainSample)
 }
 
-
 ## apply the model to testing data
-modelPreidct(para)
+modelPredict(para,Sample=para$testSample)
 
+## caller evaluation
+if(para$reportTestEvaluation==TRUE){
+  callerEvaluation(para,Sample=para$testSample)
+}
 
 
 
