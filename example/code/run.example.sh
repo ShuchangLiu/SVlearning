000ad5c5359664cab7d7c10de0eecf8b75d95855
Silvia Liu <silvia.shuchang.liu@gmail.com>
2018-06-07 11:58:19 -0400

    Add running example script

---
+++ example/code/run.example.sh
@@ -0,0 +1,15 @@
+## SVlearning example
+## Please update the file path before running
+
+## step 1: please update configuration file before running
+
+## step 2: run SVlearning
+Rscript path/to/scripts/SVlearning.r path/to/config path/to/scripts/Rsource.r
+
+## alternative step 2: run cross validation for SVlearning
+Rscript path/to/scripts/SVlearning_CV.r path/to/config path/to/scripts/Rsource.r
+
+## step 3: check results in the output folder
+
+# for more details, please refer to README file
+
